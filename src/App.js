306e1f5f824e920bc34fef7d43e2e86d2cbe8bbe
Rayyan-Karimi <rayyan.karimi.30.3@mountblue.tech>
2024-12-17 16:50:40 +0530

    Login logout using conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import { useState } from "react";
-import ButtonChild from "./ButtonChild";
-
-// Props - parent 2 child comms
-function Greeting(props) {
-  return (
-    <h2>
-      Welcome to React, <i>{props.name}</i>. Lets build something amazing.
-    </h2>
-  );
-}
-
+import React, { useState } from "react";
 function App() {
-  // State
-  const [isOn, setIsOn] = useState(false);
-  const toggleButton = () => {
-    setIsOn(!isOn);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const toggleLogin = () => {
+    setIsLoggedIn(!isLoggedIn);
   };
-
-  // Event handling
-  const handleClick = () => {
-    alert("Button was clicked.");
-  };
-
-  const [message, setMessage] = useState(""); // state to store the message
-  const handleMessage = (msg) => {
-    // update teh state wth message from child.
-    setMessage(msg);
-  };
-
   return (
     <div>
-      <h1>Hello, React!</h1>
-      <Greeting name="Rayyan" />
-      <Greeting name="Alice" />
-      { /* pass a calback fuction to the child */ }
-      <ButtonChild onButtonClick={handleMessage} />
-      { /* display the message from the child */}
-      <p>{message}</p>
-      <button onClick={toggleButton}>{isOn ? "ON" : "OFF"}</button>
-      <button onClick={handleClick}>Click me!</button>
+      <h1>Conditional Rendering in React</h1>
+      {isLoggedIn ? (
+        <div>
+          <h2>Welcome</h2>
+          <button onClick={toggleLogin}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <h2>Please login</h2>
+          <button onClick={toggleLogin}>Login</button>
+        </div>
+      )}
     </div>
   );
 }
