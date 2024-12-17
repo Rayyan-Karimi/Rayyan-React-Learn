c6da748f679de2e2819e32266754c1cc500efab3
Rayyan-Karimi <rayyan.karimi.30.3@mountblue.tech>
2024-12-17 17:19:48 +0530

    Deleting users dynamically

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const users = [
+  const [users, setUsers] = useState([
     { id: 1, name: "Ray", age: 25 },
     { id: 2, name: "Lee", age: 24 },
     { id: 3, name: "Jas", age: 23 },
     { id: 4, name: "Oly", age: 22 },
     { id: 5, name: "Cas", age: 21 },
-  ];
+  ]);
+
+  const handleDelete = (id) => {
+    const updatedUsers = users.filter((user) => user.id !== id);
+    setUsers(updatedUsers);
+  };
+
+  const listStyle = {
+    listStyleType: "none",
+    padding: 0,
+  };
+
+  const listItemStyle = {
+    margin: "10px 0",
+    padding: "10px",
+    backgroundColor: "#f9f9f9",
+    borderRadius: "5px",
+    boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+  };
 
   return (
     <div>
-      <h1>Rendering a List of Objectives</h1>
-      <ul>
+      <h1>Styled List of Objectives rendered..</h1>
+      <ul style={listStyle}>
         {users.map((user) => (
-          <li key={user.id}>
-            {user.name} is {user.age} years old.
+          <li key={user.id} style={listItemStyle}>
+            <span>
+              {user.name} is {user.age} years old.
+            </span>
+            <button onClick={() => handleDelete(user.id)}>Delete user</button>
           </li>
         ))}
       </ul>
